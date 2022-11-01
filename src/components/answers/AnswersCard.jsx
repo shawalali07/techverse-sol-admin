b07e9f1e51e2f28b3919508ed7a5ef3357a788ba
Shawal Ali <shawalnetwork@gmail.com>
2022-11-01 21:08:21 +0500

    ui working

---
+++ src/components/answers/AnswersCard.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Card from '../card/Card';
+
+const AnswersCard = () => {
+  return <Card type='Answers' />;
+};
+
+export default AnswersCard;
---
+++ src/components/questions/QuestionsCard.jsx
@@ -0,0 +1,7 @@
+import Card from '../card/Card';
+
+const QuestionsCard = () => {
+  return <Card type='Questions' />;
+};
+
+export default QuestionsCard;
